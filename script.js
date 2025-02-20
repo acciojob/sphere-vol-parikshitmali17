f6ff0b586d6d5f0e76222c0314811ae5f014611c
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-20 17:28:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-let radius = document.querySelector("#radius").value
-let v
-let volume=document.querySelector("#volume")
+
 function volume_sphere(event, radius) {
     //Write your code here
+	let radius = document.querySelector("#radius").value
 	event.preventDefault();
 	let radius = parseFloat(radiusInput);
 	if(isNaN(radius) || radius<0){
 		return NaN
 	}
-	v=4*Math.PI*Math.pow(radius,3)/3
+	v=(4/3)*Math.PI*Math.pow(radius,3)
 document.querySelector("#volume").value=v.toFixed(4)
 	
 } 
